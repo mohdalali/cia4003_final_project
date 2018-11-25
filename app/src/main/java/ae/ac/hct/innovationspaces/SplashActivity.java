624327006b605ed06899938211f7e84b11bbcf25
Mohamed Al Ali <mohdalali83@gmail.com>
2018-11-25 01:34:24 -0800

    video view

--- app/src/main/java/ae/ac/hct/innovationspaces/SplashActivity.java
+++ app/src/main/java/ae/ac/hct/innovationspaces/SplashActivity.java
@@ -12,16 +12,37 @@ import android.widget.VideoView;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private VideoView videoView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        TextureVideoView cropTextureView = (TextureVideoView) findViewById(R.id.cropTextureView);
-// Use `setScaleType` method to crop video
-        cropTextureView.setScaleType(TextureVideoView.ScaleType.TOP);
-// Use `setDataSource` method to set data source, this could be url, assets folder or path
-        cropTextureView.setDataSource("http://www.w3schools.com/html/mov_bbb.mp4");
-        cropTextureView.play();
+        videoView = findViewById(R.id.splashVideo);
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.splash);
+        videoView.setVideoURI(uri);
+        videoView.start();
+    }
+
+    private void setDimension() {
+        float videoProportion = getVideoProportion();
+        int screenWidth = getResources().getDisplayMetrics().widthPixels;
+        int screenHeight = getResources().getDisplayMetrics().heightPixels;
+        float screenProportion = (float) screenHeight / (float) screenWidth;
+        android.view.ViewGroup.LayoutParams lp = videoView.getLayoutParams();
+
+        if (videoProportion < screenProportion) {
+            lp.height= screenHeight;
+            lp.width = (int) ((float) screenHeight / videoProportion);
+        } else {
+            lp.width = screenWidth;
+            lp.height = (int) ((float) screenWidth * videoProportion);
+        }
+        videoView.setLayoutParams(lp);
+    }
+
+    private float getVideoProportion(){
+        return 1.5f;
     }
 }
