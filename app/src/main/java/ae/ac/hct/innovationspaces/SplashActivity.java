aced4ba6a2d8d117e21db21ae4d0e172414f217f
Mohamed Al Ali <mohdalali83@gmail.com>
2018-11-25 02:14:01 -0800

    final -space

--- app/src/main/java/ae/ac/hct/innovationspaces/SplashActivity.java
+++ app/src/main/java/ae/ac/hct/innovationspaces/SplashActivity.java
@@ -12,38 +12,11 @@ import android.widget.VideoView;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private VideoView videoView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        videoView = findViewById(R.id.splashVideo);
-        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.splash);
-        videoView.setVideoURI(uri);
-        videoView.start();
-        setDimension();
-    }
 
-    private void setDimension() {
-        float videoProportion = getVideoProportion();
-        int screenWidth = getResources().getDisplayMetrics().widthPixels;
-        int screenHeight = getResources().getDisplayMetrics().heightPixels;
-        float screenProportion = (float) screenHeight / (float) screenWidth;
-        android.view.ViewGroup.LayoutParams lp = videoView.getLayoutParams();
-
-        if (videoProportion < screenProportion) {
-            lp.height= screenHeight;
-            lp.width = (int) ((float) screenHeight / videoProportion);
-        } else {
-            lp.width = screenWidth;
-            lp.height = (int) ((float) screenWidth * videoProportion);
-        }
-        videoView.setLayoutParams(lp);
-    }
-
-    private float getVideoProportion(){
-        return 1.5f;
     }
 }
